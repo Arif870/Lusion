40e0600267e1c2159497c1b9a25a68480bdd6336
Arif870 <arifuzzaman959@gmail.com>
2022-01-06 16:08:39 +0600

    single product page design done

--- src/Components/Pages/SingleProduct/SingleProduct.js
+++ src/Components/Pages/SingleProduct/SingleProduct.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./SingleProduct.css";
 
 const SingleProduct = () => {
   let { productId } = useParams();
@@ -13,33 +14,48 @@ const SingleProduct = () => {
   }, []);
   return (
     <>
-      <div className="container mt-5">
-        <div className="row">
+      <div className="container mt-5 singleProduct">
+        <div className="row d-flex flex-wrap align-items-center">
           <div className="col">
-            <img src="https://i.ibb.co/BjpB53W/nuts.png" alt="" />
+            <img
+              style={{ width: "100%" }}
+              src="https://i.ibb.co/BjpB53W/nuts.png"
+              alt=""
+            />
+          </div>
+          <div className="col">
+            <h1>nedgu nuts</h1>
+            <p className="mt-4">
+              Almond Nuts of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to
+              make a type specimen.
+            </p>
+            <p className="text-success">32 in stock 😃</p>
+            <p className="fs-4">Price: $20</p>
             <div className="d-flex align-items-center">
-              <span
-                className="rounded"
-                style={{
-                  border: ` 1px solid var(--primary-color)`,
-                  marginRight: "10px",
-                  padding: "10px",
-                  cursor: "pointer",
-                }}
-              >
-                <i className="fas fa-plus"></i>
+              <span>
+                <i
+                  className="fas fa-plus rounded-circle"
+                  style={{
+                    border: ` 1px solid var(--primary-color)`,
+                    marginRight: "10px",
+                    padding: "10px",
+                    cursor: "pointer",
+                  }}
+                ></i>
               </span>
               <span className="p-3">1</span>
-              <span
-                className="rounded"
-                style={{
-                  border: `1px solid var(--primary-color)`,
-                  marginLeft: "10px",
-                  padding: "10px",
-                  cursor: "pointer",
-                }}
-              >
-                <i className="fas fa-minus"></i>
+              <span>
+                <i
+                  className="fas fa-minus rounded-circle"
+                  style={{
+                    border: ` 1px solid var(--primary-color)`,
+                    marginLeft: "10px",
+                    padding: "10px",
+                    cursor: "pointer",
+                  }}
+                ></i>
               </span>
 
               <button
@@ -52,15 +68,8 @@ const SingleProduct = () => {
                 Add to Cart
               </button>
             </div>
-          </div>
-          <div className="col">
-            <h1>nedgu nuts</h1>
-            <p>
-              Almond Nuts of the printing and typesetting industry. Lorem Ipsum
-              has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to
-              make a type specimen.
-            </p>
+            <hr />
+            <p className="mt-5 fs-4">Category : Vegetables</p>
           </div>
         </div>
       </div>
